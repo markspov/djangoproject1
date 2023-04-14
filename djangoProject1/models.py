1370b9ffd31d3a5e87951deb31653ea10ec2e50c
mvrkspov <markolendirangu@gmail.com>
2023-04-14 16:24:02 +0300

    initial commit

---
+++ djangoProject1/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    publication_date = models.DateField()
+    genre = models.CharField(max_length=100)
